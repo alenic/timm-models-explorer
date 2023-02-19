b36df68e170f2e33b3e5b8fa7d4eaa44268dda4d
alenic <nicolosi.ale@gmail.com>
2023-02-19 16:39:18 +0100

    first

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,23 @@
 import streamlit as st
+import plotly.express as px
+import pandas as pd
 
 st.title('🎈 App Name')
 
-st.write('Hello world!')
+df = pd.read_csv("data/results-imagenet.csv")
+df["param_count"] = df["param_count"].str.replace(",","").astype(float)
+
+
+fig = px.scatter(
+    df.query("top1>=89.0"),
+    x="img_size",
+    y="top1",
+    size="param_count",
+    #color="continent",
+    hover_name="model",
+    #log_x=True,
+    size_max=60,
+)
+
+
+st.plotly_chart(fig, theme="streamlit", use_container_width=True)
